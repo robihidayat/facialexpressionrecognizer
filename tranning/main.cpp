706d8168d900c69af5e9e5c1e867a60a07ce402c
Robi Hidayat <robi.hidayat@mail.ugm.ac.id>
2015-08-13 13:11:53 +0700

    Update main.cpp

--- tranning/main.cpp
+++ tranning/main.cpp
@@ -1,1 +1,54 @@
-asac
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/contrib/contrib.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+ // library svm
+
+
+/*
+-lopencv_core -lopencv_contrib -lopencv_highgui -lopencv_imgproc -lopencv_objdetect  */
+
+/*header files*/
+#include "recognize.h"
+
+using namespace std;
+using namespace cv;
+
+  
+
+
+int main()
+{
+
+ 
+   /*starting training millih salah satu*/
+
+//cout<<"eigen start training ... !!"<<endl;
+//eigenFaceTrainer();
+//FisherFaceTrainer();
+//cout<<"lbp start training ... !!"<<endl;
+//LBPHFaceTrainer();
+//cout<<"finish training ... !!"<<endl;
+
+    /*starting recognize*/
+   //cout<<"......STARTING recognize ..........."<<endl;
+
+//int value2 = EigenRecognition();
+//int value3 = FisherRecognition();
+//int value1 = LBPHRecognition();
+//pengujian();
+ 
+	
+
+//////// \\\\\\\\\
+
+
+	return 0;
+
+}
+
